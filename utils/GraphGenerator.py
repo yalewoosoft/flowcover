63b9b2cc716c4711f69e8358dbb29ffbcbdadb00
Yanxiu Wuwang <uofvo@student.kit.edu>
2024-05-28 14:46:38 +0200

    waxman generation
    Signed-off-by: Yanxiu Wuwang <uofvo@student.kit.edu>

--- utils/GraphGenerator.py
+++ utils/GraphGenerator.py
@@ -1,11 +1,12 @@
 from networkx import Graph
 from itertools import product
-from random import random
+from random import random, choice
+from math import sqrt, ceil, exp
 def erdos_renyi_generator(n: int, p: float) -> Graph:
     g = Graph()
     g.add_nodes_from(range(1, n+1))
     for s1, s2 in product(range(1, n + 1), range(1, n + 1)):
-        if s1 == s2:
+        if s1 >= s2:
             continue
         x = random()
         if x <= p:
@@ -17,4 +18,51 @@ def linear_generator(n: int) -> Graph:
     g.add_nodes_from(range(1, n+1))
     for i in range(1, n):
         g.add_edge(i, i+1)
+    return g
+
+def waxman_generator_1(n: int, alpha: float, beta: float) -> Graph:
+    g = Graph()
+    g.add_nodes_from(range(1, n + 1))
+    side_length = ceil(sqrt(n))
+    x_coords = list(range(0, side_length))
+    y_coords = list(range(0, side_length))
+    coords: dict[int, (int, int)] = {}
+    for i in range(1, n+1):
+        x = choice(x_coords)
+        x_coords.remove(x)
+        y = choice(y_coords)
+        y_coords.remove(y)
+        coords[i] = (x, y)
+    dist: dict[(int, int), float] = {}
+    for i, j in product(range(1, n+1), range(1, n+1)):
+        i_coords = coords[i]
+        j_coords = coords[j]
+        dist_i_j = sqrt((i_coords[0] - j_coords[0])**2 + (i_coords[1] - j_coords[1])**2)
+        dist[(i, j)] = dist_i_j
+    L = max(dist.values())
+    for i, j in product(range(1, n + 1), range(1, n + 1)):
+        if i >= j:
+            continue
+        p = beta * exp(-dist[(i, j)] / (L * alpha))
+        x = random()
+        if x <= p:
+            g.add_edge(i, j)
+    return g
+
+def waxman_generator_2(n: int, alpha: float, beta: float, L: float) -> Graph:
+    g = Graph()
+    g.add_nodes_from(range(1, n + 1))
+    dist: dict[(int, int), float] = {}
+    for i, j in product(range(1, n+1), range(1, n+1)):
+        if i == j:
+            dist[(i, j)] = 0
+        else:
+            dist[(i, j)] = random() * L
+    for i, j in product(range(1, n + 1), range(1, n + 1)):
+        if i >= j:
+            continue
+        p = beta * exp(-dist[(i, j)] / (L * alpha))
+        x = random()
+        if x <= p:
+            g.add_edge(i, j)
     return g
