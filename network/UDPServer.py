e7f7d2078b5845a737f4b2b87bcfe3c645451dca
Yanxiu Wuwang <uofvo@student.kit.edu>
2024-06-25 14:40:43 +0200

    collect statistics of trafgen packets
    Signed-off-by: Yanxiu Wuwang <uofvo@student.kit.edu>

--- network/UDPServer.py
+++ network/UDPServer.py
@@ -8,13 +8,13 @@ def handle_signal(sig, frame):
     filename = f'/tmp/trafgen_{flow_id}.log'
     print(f'Flow {flow_id}: {num_bytes} received.')
     with open(filename, 'w') as f:
-        f.write(str(num_bytes))
+        f.write(str(num_bytes // 38 * 100))
     sys.exit()
 signal.signal(signal.SIGINT, handle_signal)
 signal.signal(signal.SIGTERM, handle_signal)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 flow_id = int(sys.argv[1])
-server_socket.bind(('', int(sys.argv[2])))
+server_socket.bind(('::', int(sys.argv[2])))
 while True:
-    message, address = server_socket.recv(1024)
+    message = server_socket.recv(1024)
     num_bytes += len(message)
