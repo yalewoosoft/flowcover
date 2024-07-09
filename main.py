976ab046152e6c74f200cb6993cca736690235b1
Zhi Wang <ubwsx@student.kit.edu>
2024-07-09 16:13:45 +0200

    clean branch and add figure9 0.01 0.05

--- main.py
+++
@@ -1,9 +0,0 @@
-from mininet.cli import CLI
-from network import SimulatedNetwork
-from controller import Controller
-
-if __name__ == '__main__':
-    net = SimulatedNetwork.main()
-    Controller.main() # start controller
-    CLI(net) # drop to mininet console for debugging
-    net.stop()
